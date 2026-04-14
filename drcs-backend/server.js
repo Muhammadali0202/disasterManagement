033c85826940c128657d89e331c426108eabedaa
user.name like:"Muhammadali0202 <user.email like:"muhammadalisajjadakhunzada@gmail.com>
2026-04-14 12:29:04 +0500

    fixed the initlization of the app.use()

--- drcs-backend/server.js
+++ drcs-backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql2/promise');
 require('dotenv').config();
+
 app.use(express.json());
 
 // 1. Initialize the App
